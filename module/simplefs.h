123b24bd40cd7b61e1d9bf57030fd45e05f731c0
Жвирблинский Ярослав <153995684+Zoomby2@users.noreply.github.com>
2026-05-22 08:55:15 +0300

    Update simplefs.h

--- module/simplefs.h
+++ module/simplefs.h
@@ -6,25 +6,27 @@
 
 #define SIMPLEFS_MAGIC 0x534D504C
 
+/* Суперблок на диске */
 struct simplefs_super_block {
     __le32 magic;
     __le32 version;
     __le32 hash;
-    __le64 files_count;
-    __le64 block_size;
-    __le64 max_file_sectors;
+    __le64 f_count;   /* кол-во файлов */
+    __le64 b_size;    /* размер сектора */
+    __le64 max_sec;   /* макс. секторов на файл */
 };
 
+/* Маппинг для IOCTL */
 struct simplefs_mapping {
-    char filename[256];
-    __u64 start_sector;
-    __u64 sector_count;
+    char name[32];
+    __u64 start_sec;
+    __u64 sec_cnt;
 };
 
 #define SIMPLEFS_IOC_MAGIC 'S'
 #define SIMPLEFS_IOC_ZERO_FILES _IO(SIMPLEFS_IOC_MAGIC, 1)
 #define SIMPLEFS_IOC_ERASE_FS   _IO(SIMPLEFS_IOC_MAGIC, 2)
-#define SIMPLEFS_IOC_GET_HASHES _IOR(SIMPLEFS_IOC_MAGIC, 3, __u32)
+#define SIMPLEFS_IOC_GET_HASHES _IOR(SIMPLEFS_IOC_MAGIC, 3, __u32[10])
 #define SIMPLEFS_IOC_GET_MAP    _IOWR(SIMPLEFS_IOC_MAGIC, 4, struct simplefs_mapping)
 
 #endif
