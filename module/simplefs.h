27257dd780a79aa7afda85590b4a69406ecf5dfd
Жвирблинский Ярослав <153995684+Zoomby2@users.noreply.github.com>
2026-06-01 00:16:29 +0300

    Update simplefs.h

--- module/simplefs.h
+++ module/simplefs.h
@@ -4,6 +4,10 @@
 #include <linux/ioctl.h>
 #include <linux/types.h>
 
+#ifndef __user
+#define __user
+#endif
+
 #define SIMPLEFS_MAGIC 0x534D504C
 
 /* Структура суперблока файловой системы */
@@ -23,10 +27,17 @@ struct simplefs_mapping {
     __u64 sector_count;
 };
 
+/* Структура для передачи динамического массива хэшей */
+struct simplefs_hash_list {
+    __u32 __user *list;
+    __u32 count;
+};
+
 #define SIMPLEFS_IOC_MAGIC 'S'
 #define SIMPLEFS_IOC_ZERO_FILES _IO(SIMPLEFS_IOC_MAGIC, 1)
 #define SIMPLEFS_IOC_ERASE_FS   _IO(SIMPLEFS_IOC_MAGIC, 2)
-#define SIMPLEFS_IOC_GET_HASHES _IOR(SIMPLEFS_IOC_MAGIC, 3, __u32[10]) 
+#define SIMPLEFS_IOC_GET_HASHES _IOWR(SIMPLEFS_IOC_MAGIC, 3, struct simplefs_hash_list)
 #define SIMPLEFS_IOC_GET_MAP    _IOWR(SIMPLEFS_IOC_MAGIC, 4, struct simplefs_mapping)
+#define SIMPLEFS_IOC_GET_FCOUNT _IOR(SIMPLEFS_IOC_MAGIC, 5, __u64)
 
 #endif
