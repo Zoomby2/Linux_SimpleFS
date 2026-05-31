2c91286dc5bc18eb5f79340e52588fabeec952f0
Жвирблинский Ярослав <153995684+Zoomby2@users.noreply.github.com>
2026-06-01 00:19:23 +0300

    Refactor file handling and IOCTL calls in test_app

--- module/test_app.c
+++ module/test_app.c
@@ -11,64 +11,84 @@
 int main(int argc, char *argv[]) {
     srand(time(NULL));
 
-    if (argc > 1) {
-        int fd = open("/mnt/simplefs", O_RDONLY);
-        if (fd < 0) {
-            perror("Не удалось открыть каталог ФС");
-            return 1;
-        }
+    int fd = open("/mnt/simplefs", O_RDONLY);
+    if (fd < 0) {
+        perror("Не удалось открыть каталог ФС");
+        return 1;
+    }
 
+    /* Запрашиваю реальное количество файлов у ядра */
+    uint64_t files_count = 0;
+    if (ioctl(fd, SIMPLEFS_IOC_GET_FCOUNT, &files_count) < 0) {
+        perror("Ошибка при получении количества файлов");
+        close(fd);
+        return 1;
+    }
+
+    if (argc > 1) {
         if (strcmp(argv[1], "--zero") == 0) {
             ioctl(fd, SIMPLEFS_IOC_ZERO_FILES);
-            printf("Файлы успешно обнулены через IOCTL.\n");
+            printf("Файлы успешно обнулены.\n");
         } else if (strcmp(argv[1], "--erase") == 0) {
             ioctl(fd, SIMPLEFS_IOC_ERASE_FS);
             printf("ФС стерта. Доступ к директории заблокирован ядром.\n");
         } else if (strcmp(argv[1], "--hashes") == 0) {
-            __u32 h[10]; 
-            if (ioctl(fd, SIMPLEFS_IOC_GET_HASHES, h) < 0) {
-                perror("Ошибка при вызове IOCTL_GET_HASHES");
-                close(fd);
+            struct simplefs_hash_list hl;
+            hl.count = files_count;
+            hl.list = malloc(files_count * sizeof(__u32));
+            if (!hl.list) {
+                perror("Ошибка выделения памяти");
                 return 1;
             }
-            printf("----- Список хэшей файлов -----\n");
-            for (int j = 0; j < 10; j++) {
-                printf("file%d: хэш 0x%X\n", j, h[j]);
+
+            if (ioctl(fd, SIMPLEFS_IOC_GET_HASHES, &hl) < 0) {
+                perror("Ошибка при вызове IOCTL_GET_HASHES");
+            } else {
+                printf("----- Список хэшей (%llu файлов) -----\n", (unsigned long long)files_count);
+                for (int j = 0; j < files_count; j++) {
+                    printf("file%d: хэш 0x%X\n", j, hl.list[j]);
+                }
             }
+            free(hl.list);
         } else if (strcmp(argv[1], "--map") == 0 && argc > 2) {
             struct simplefs_mapping map;
             strncpy(map.filename, argv[2], sizeof(map.filename));
-            ioctl(fd, SIMPLEFS_IOC_GET_MAP, &map);
-            printf("Файл %s: начальный сектор = %llu, секторов = %llu\n", map.filename, (unsigned long long)map.start_sector, (unsigned long long)map.sector_count);
+            if (ioctl(fd, SIMPLEFS_IOC_GET_MAP, &map) < 0) {
+                perror("Ошибка IOCTL_GET_MAP");
+            } else {
+                printf("Файл %s: начальный сектор = %llu, секторов = %llu\n", 
+                       map.filename, (unsigned long long)map.start_sector, (unsigned long long)map.sector_count);
+            }
         }
         close(fd);
         return 0;
     }
 
+    printf("Найдено файлов на диске: %llu. Запуск теста чтения/записи:\n", (unsigned long long)files_count);
     char path[64];
-    int i, fd;
+    int i, f;
     int wr_val, rd_val;
 
-    printf("Запуск автоматического теста чтения/записи для файлов:\n");
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < files_count; i++) {
         snprintf(path, sizeof(path), "/mnt/simplefs/file%d", i);
-        fd = open(path, O_RDWR);
-        if (fd < 0) {
-            printf("/mnt/simplefs/file%d открыть не удалось (возможно, ФС стерта)\n", i);
+        f = open(path, O_RDWR);
+        if (f < 0) {
+            printf("file%d открыть не удалось\n", i);
             continue;
         }
 
         wr_val = rand() % 1000;
-        write(fd, &wr_val, sizeof(wr_val));
+        write(f, &wr_val, sizeof(wr_val));
 
-        lseek(fd, 0, SEEK_SET);
-        if (read(fd, &rd_val, sizeof(rd_val)) > 0 && rd_val == wr_val) {
+        lseek(f, 0, SEEK_SET);
+        if (read(f, &rd_val, sizeof(rd_val)) > 0 && rd_val == wr_val) {
             printf("file%d: записано '%d', прочитано '%d' -> ОК\n", i, wr_val, rd_val);
         } else {
             printf("file%d: записано '%d', прочитано '%d' -> Провал\n", i, wr_val, rd_val);
         }
-        close(fd);
+        close(f);
     }
 
+    close(fd);
     return 0;
 }
